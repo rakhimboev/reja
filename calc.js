f1181f57a67426f322ed95b88453ab2f4c1d8379
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-09-18 23:25:49 -0400

    modules done

---
+++ calc.js
@@ -0,0 +1,15 @@
+let calc = {}
+calc.multiply = (a, b) => {
+    return a * b
+}
+calc.devide = (a, b) => {
+    return a / b
+}
+calc.plus = (a, b) => {
+    return a + b
+}
+calc.minus = (a, b) => {
+    return a - b
+}
+
+module.exports = calc;
