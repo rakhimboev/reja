947fdc10a34be34c63d4b63e54b7cc143f1c039c
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-09-26 02:17:18 -0400

    BRR: async functions

--- train.js
+++ train.js
@@ -6,25 +6,36 @@ const list = [
     'ishchilaringiz ustida ishlang', //40-50
     'boldi endi ee, dam oling' //50
 ]
-
-const advice = (a, callBack) => {
-    if (typeof a !== 'number') callBack('insert number', null)
-    else if (a <= 20) callBack(null, list[0])
-    else if (a > 20 && a <= 30) callBack(null, list[1])
-    else if (a > 30 && a <= 40) callBack(null, list[2])
-    else if (a > 40 && a <= 50) callBack(null, list[3])
+const advice = async (a) => {
+    if (typeof a !== 'number') throw new Error('insert number')
+    else if (a <= 20) return list[0]
+    else if (a > 20 && a <= 30) return list[1]
+    else if (a > 30 && a <= 40) return list[2]
+    else if (a > 40 && a <= 50) return list[3]
     else {
-        setTimeout(() => {
-            callBack(null, list[4])
-        }, 5000)
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[4])
+            }, 5000)
+        })
     }
 }
 
-console.log('passed here 0')
-advice(55, (err, data) => {
-    if (err) console.log('error', err)
-    else {
-        console.log('javob: ', data)
-    }
-})
-console.log('passed here 1')
+// console.log('passed here 0')
+// advice(55, (err, data) => {
+//     if (err) console.log('error', err)
+//     else {
+//         console.log('javob: ', data)
+//     }
+// })
+// console.log('passed here 1')
+
+const run = async () => {
+    let result = await advice(25)
+    console.log(result)
+    result = await advice(70)
+    console.log(result)
+    result = await advice(41)
+    console.log(result)
+}
+run()
