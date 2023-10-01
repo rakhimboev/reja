e0008bedbba5bea7c0d2e48a6657922de7f89606
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-09-30 22:30:48 -0400

    BRR: Portfolio mission accomplished

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ console.log('Web Server started')
 const express = require('express')
 const res = require('express/lib/response')
 const app = express()
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log('Error: ', err)
+    } else {
+        user = JSON.parse(data)
+    }
+})
 
 const db = require('./server').db()
 
--- train.js
+++ train.js
@@ -1,25 +1,36 @@
-console.log('Jack Ma maslahatlari: ')
-const list = [
-    'Yaxshi oqing', //0-20
-    'yaxshi dam oling', //20-30
-    'oz ustingizda ishlang', //30-40
-    'ishchilaringiz ustida ishlang', //40-50
-    'boldi endi ee, dam oling' //50
-]
-const advice = async (a) => {
-    if (typeof a !== 'number') throw new Error('insert number')
-    else if (a <= 20) return list[0]
-    else if (a > 20 && a <= 30) return list[1]
-    else if (a > 30 && a <= 40) return list[2]
-    else if (a > 40 && a <= 50) return list[3]
-    else {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(list[4])
-            }, 5000)
-        })
-    }
+// E-task
+const reversing = (str) => {
+    return str.split('').reverse().join('');
 }
+
+const test = reversing('hello')
+console.log(test)
+
+
+
+
+// console.log('Jack Ma maslahatlari: ')
+// const list = [
+//     'Yaxshi oqing', //0-20
+//     'yaxshi dam oling', //20-30
+//     'oz ustingizda ishlang', //30-40
+//     'ishchilaringiz ustida ishlang', //40-50
+//     'boldi endi ee, dam oling' //50
+// ]
+// const advice = async (a) => {
+//     if (typeof a !== 'number') throw new Error('insert number')
+//     else if (a <= 20) return list[0]
+//     else if (a > 20 && a <= 30) return list[1]
+//     else if (a > 30 && a <= 40) return list[2]
+//     else if (a > 40 && a <= 50) return list[3]
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[4])
+//             }, 5000)
+//         })
+//     }
+// }
 
 // console.log('passed here 0')
 // advice(55, (err, data) => {
@@ -30,12 +41,12 @@ const advice = async (a) => {
 // })
 // console.log('passed here 1')
 
-const run = async () => {
-    let result = await advice(25)
-    console.log(result)
-    result = await advice(70)
-    console.log(result)
-    result = await advice(41)
-    console.log(result)
-}
-run()
+// const run = async () => {
+//     let result = await advice(25)
+//     console.log(result)
+//     result = await advice(70)
+//     console.log(result)
+//     result = await advice(41)
+//     console.log(result)
+// }
+// run()
