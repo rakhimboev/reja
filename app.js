00d69a6fc36a0c5cfcdc03bb2fef575d5d1c03ce
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-10-03 00:31:43 -0400

    BRR: CRUD operations

--- app.js
+++ app.js
@@ -27,16 +27,33 @@ app.set('view engine', 'ejs')
 
 // 4 routing code
 app.post('/create-item', (req, res) => {
-    console.log(req.body)
-    res.json({ test: 'success' })
+    console.log('user is here: /create-item')
+    const new_reja = req.body.reja
+    db.collection('plans').insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.end('something went wrong')
+        } else {
+            res.end('successully created')
+        }
+    })
 })
 
 app.get('/', (req, res) => {
-    res.render('reja')
+    console.log('user is here: /')
+    db.collection('plans').find().toArray((err, data) => {
+        if (err) {
+            console.log(err)
+            res.end('Something went wrong')
+        } else {
+            // console.log(data)
+            res.render('reja', {items: data})
+        }
+    })
 })
 
 app.get('/author', (req, res) => {
-    res.render('author', {user: user})
+    res.render('author', { user: user })
 })
 
 module.exports = app
