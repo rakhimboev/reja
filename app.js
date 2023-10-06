7027c5daf5700f509ea88c55ce1ef031c4ce9ca4
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-10-05 22:08:11 -0400

    BRR: Reja project accomplished

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const res = require('express/lib/response')
 const app = express()
 const fs = require('fs');
+const mongodb = require('mongodb')
 
 let user;
 fs.readFile("database/user.json", "utf-8", (err, data) => {
@@ -29,10 +30,37 @@ app.set('view engine', 'ejs')
 app.post('/create-item', (req, res) => {
     console.log('user is here: /create-item')
     const new_reja = req.body.reja
-    db.collection('plans').insertOne({reja: new_reja}, (err, data) => {
-        console.log(data.ops)
+    db.collection('plans').insertOne({ reja: new_reja }, (err, data) => {
         res.json(data.ops[0])
     })
+})
+
+app.post('/delete-item', (req, res) => {
+    const id = req.body.id
+    db.collection('plans').deleteOne(
+        { _id: new mongodb.ObjectId(id) },
+        (err, data) => {
+            res.json({ state: 'success' })
+        }
+    );
+})
+
+app.post('/edit-item', (req, res) => {
+    const data = req.body
+    console.log(data)
+    db.collection('plans').findOneAndUpdate(
+        { _id: new mongodb.ObjectId(data.id) },
+        { $set: { reja: data.new_input } }, (err, data) => {
+            res.json({ state: 'success' })
+        })
+})
+
+app.post('/delete-all', (req, res) => {
+    if (req.body.delete_all) {
+        db.collection('plans').deleteMany(() => {
+            res.json({ state: 'Do you want to delete all plans' })
+        })
+    }
 })
 
 app.get('/', (req, res) => {
@@ -42,8 +70,7 @@ app.get('/', (req, res) => {
             console.log(err)
             res.end('Something went wrong')
         } else {
-            // console.log(data)
-            res.render('reja', {items: data})
+            res.render('reja', { items: data })
         }
     })
 })
--- public/browser.js
+++ public/browser.js
@@ -3,7 +3,7 @@ console.log('Running Front end')
 const itemTemplate = (item) => {
     return `
     <li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
-    <span class="item-text">${item.reja}    </span>
+    <span class="item-text">${item.reja}</span>
     <div>
         <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
         <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
@@ -20,9 +20,49 @@ document.getElementById('create-form')
         axios.post('/create-item', { reja: createField.value })
             .then((response) => {
                 document.getElementById('item-list')
-                    .insertAdjacentHTML('beforeend', itemTemplate(response.date))
+                    .insertAdjacentHTML('beforeend', itemTemplate(response.data))
                 createField.value = ''
                 createField.focus()
             })
             .catch((err) => { console.log('try one more time') })
     })
+
+document.addEventListener('click', (e) => {
+    //delete operation
+    if (e.target.classList.contains('delete-me')) {
+        if (confirm('Are you sure you want to delete?')) {
+            axios.post('/delete-item', { id: e.target.getAttribute('data-id') })
+                .then((response) => {
+                    console.log(response.data)
+                    e.target.parentElement.parentElement.remove()
+                })
+                .catch((err) => { console.log('please, try again') })
+        }
+    }
+    //edit operation
+    if (e.target.classList.contains('edit-me')) {
+        let userInput = prompt(
+            'Edit the plan',
+            e.target.parentElement.parentElement.querySelector('.item-text').innerHTML)
+        if (userInput) {
+            axios.post('/edit-item', {
+                id: e.target.getAttribute('data-id'),
+                new_input: userInput
+            }).then(response => {
+                console.log(response.data);
+                e.target.parentElement.parentElement.querySelector(
+                    '.item-text')
+                    .innerHTML = userInput
+            }).catch((err) => {
+                console.log('Please try again');
+            })
+        }
+    }
+});
+
+document.getElementById('clean-all').addEventListener('click', () => {
+    axios.post('delete-all', { delete_all: true }).then(response => {
+        alert(response.data.state)
+        document.location.reload();
+    });
+})
