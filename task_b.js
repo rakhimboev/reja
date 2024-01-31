40765c14f82b931a9a94d7ce298a4f9976a3f532
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-01-30 23:25:42 -0500

    new reja

--- task_b.js
+++
@@ -1,8 +0,0 @@
-function getDigits(word) {
-    const nums = word.match(/[0-9]/g);
-    
-    console.log(`Length: ${nums ? nums.length : 0}  digits: ${ nums.join(",")}`);
-  }
-  
-  getDigits("ag1aw5g6")
-  getDigits("qulpi7ni8sa9")
