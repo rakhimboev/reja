d0c51b8541eeb3c777b8fa6d60e7a66ce5bcc44c
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-01-31 01:44:12 -0500

    External modules

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // Core Module packages
 
-let num = 0
+// let num = 0
+// setInterval(() => {
+//     num++
+//     // console.log('Count: ', num)
+// }, 300);
+
+// const fs = require('fs')
+// const data = fs.readFileSync('./input.txt', 'utf8')
+// console.log(data)
+
+// fs.writeFileSync('./input.txt', `${data} \n\t by Developer`)
+// const dataa = fs.readFileSync('./input.txt', 'utf8')
+// console.log(dataa)
+
+// External module package
+const moment = require('moment')
 setInterval(() => {
-    num++
-    // console.log('Count: ', num)
-}, 300);
+    const time = moment().format()
+    console.log(`TIme is: ${time})`)
+}, 3000);
+// console.log(time)
 
-const fs = require('fs')
-const data = fs.readFileSync('./input.txt', 'utf8')
-console.log(data)
 
-fs.writeFileSync('./input.txt', `${data} \n\t by Developer`)
-const dataa = fs.readFileSync('./input.txt', 'utf8')
-console.log(dataa)
+
 
