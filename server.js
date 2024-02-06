cd79e2996283108184f207819f9c279273c2bfd2
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-02-06 09:40:31 -0500

    feat: developed author router page

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 console.log('web server started')
 const express = require('express')
 const http = require('http')
+const fs = require('fs')
 const app = express()
+
+let user;
+fs.readFile('database/user.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log('error: ', err)
+    } else {
+        user = JSON.parse(data)
+    }
+})
 
 // step 1: Starting code
 app.use(express.static('public'))
@@ -16,7 +26,11 @@ app.set('view engine', 'ejs')
 // step 4: routing code
 app.post('/create-item', (req, res) => {
     console.log(req)
-    res.json({test: 'success'})
+    res.json({ test: 'success' })
+})
+
+app.get('/author', (req, res) => {
+    res.render('author', { user: user })
 })
 
 app.get('/', (req, res) => {
