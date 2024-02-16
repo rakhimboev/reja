27e98fb04a3394d8354f4d5e222853d4e0fa3cbf
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-02-15 23:02:45 -0500

    front end js building

--- app.js
+++ app.js
@@ -1,60 +1,55 @@
-console.log('web server started')
-const express = require('express')
-const fs = require('fs')
-const app = express()
+console.log("web server started");
+const express = require("express");
+const fs = require("fs");
+const app = express();
 
 // MongoDB callling
-const db = require('./server').db()
+const db = require("./server").db();
 
 let user;
-fs.readFile('database/user.json', 'utf8', (err, data) => {
-    if (err) {
-        console.log('error: ', err)
-    } else {
-        user = JSON.parse(data)
-    }
-})
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("error: ", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // step 1: Starting code
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // step 2: session code
 // step 3: views code
-app.set('views', 'views')
-app.set('view engine', 'ejs')
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 // step 4: routing code
-app.post('/create-item', (req, res) => {
-    console.log('user is here /create-item')
+app.post("/create-item", (req, res) => {
+  console.log("user is here /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    res.json(data.ops[0]);
+  });
+});
 
-    console.log(req.body)
-    const new_reja = req.body.reja
-    db.collection('plans').insertOne({ reja: new_reja }, (err, data) => {
-        if (err) {
-            console.log(err)
-            res.end('something went wrong')
-        } else {
-            res.end('successfully added')
-        }
-    })
-})
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
 
-app.get('/author', (req, res) => {
-    res.render('author', { user: user })
-})
+app.get("/", (req, res) => {
+  console.log("user is here /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
+});
 
-app.get('/', (req, res) => {
-    console.log('user is here /')
-    db.collection('plans').find().toArray((err, data) => {
-        if (err) {
-            console.log(err)
-            res.end('something went wrong')
-        } else {
-            res.render('reja', { items: data })
-        }
-    })
-})
-
-module.exports = app
+module.exports = app;
